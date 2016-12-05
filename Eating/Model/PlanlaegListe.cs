62ecfa3e655c90320c6520c5e7603857a263a2f2
Rudi Persson <rudi0234@edu.easj.dk>
2016-12-05 12:26:25 +0100

    En liste er nu tilføjet

---
+++ Eating/Model/PlanlaegListe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Eating.Model
+{
+    public class PlanlaegListe : ObservableCollection<Planlaeg>
+    {
+        public PlanlaegListe() : base()
+        {
+            this.Add(new Planlaeg() { Ret = "Kylling", ChefKok="Mathias", Hjaelpere = "Natascha", Oprydere="Rudi" });
+        }
+    }
+}
---
+++ Eating/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Eating.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+       public Model.PlanlaegListe PlanlaegListe { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public MainViewModel()
+        {
+            PlanlaegListe = new Model.PlanlaegListe();
+        }
+
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
