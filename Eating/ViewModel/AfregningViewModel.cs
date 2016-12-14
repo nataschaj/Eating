1850f21265d912197a7c34f356056f75c6e3a571
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-12-14 13:05:05 +0100

    pris

---
+++ Eating/ViewModel/AfregningViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Windows.Storage;
+
+namespace Eating.ViewModel
+{
+    class AfregningViewModel : INotifyPropertyChanged
+    {
+        public int pris { get; set; }
+
+
+        public AfregningViewModel()
+        {
+            this.pris = pris;
+        }
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+
+
+
+}
